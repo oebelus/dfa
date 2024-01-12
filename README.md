1067834290c1ae6f1bdee061734f7a3f6c674a54
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-01-12 21:33:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Implementation of DFA
+A simple implementation of a DFA, it asks the user for the number of states, the states, the initial state, the final state, the number of keys, and the keys as inputs to build an adjacency dictionary. It also takes test strings to see if they reach the final state. 
+
+## How to Run
+I’ll take a DFA that returns `Yes` when the test string starts with ‘1’;
+```
+A [1, B]  [0, C]
+B [1, B]  [0, B]
+C [1, C]  [0, C]
+```
+1. When prompted, enter the number of states.
+```
+>> 3
+```
+2. Enter each state.
+```
+>> A
+>> B
+>> C
+```
+3. Enter the initial state.
+```
+>> A
+```
+6. Enter the final state.
+```
+>> B
+```
+7. Enter the number of keys.
+```
+>> 2
+```
+8. Enter each key.
+```
+>> 1
+>> 0
+```
+9. Enter the relationship between the states
+```
+A 1
+>> B
+A 0
+>> C
+B 1
+```
+... etc.
+10. You can then enter test strings to see if they reach the final state.
+```
+>> 10
+Yes
+>> 0111
+No
+```
---
+++ dfa/Program.cs
@@ -0,0 +1,75 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+using System.Collections.Generic;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of states");
+        string statesCount = Console.ReadLine();
+        List <string> states = new();
+        Console.WriteLine("Enter the states");
+        for (int i = 0; i < int.Parse(statesCount); i++) {
+            string state = Console.ReadLine();
+            states.Add(state);
+        }
+        Console.WriteLine("Enter the initial state");
+        string firstState = Console.ReadLine();
+        Console.WriteLine("Enter the final state");
+        string finalState = Console.ReadLine();
+        Console.WriteLine("Enter the number of keys");
+        string keysCount = Console.ReadLine();
+        List <string> keys = new();
+        Console.WriteLine("Enter the keys");
+        for (int i = 0; i < int.Parse(keysCount); i++) {
+            string key = Console.ReadLine();
+            keys.Add(key);
+        }
+        
+        Dictionary <string, Dictionary<string, string>> adjacency = new();
+        foreach (string state in states) {
+            if (!adjacency.ContainsKey(state)) adjacency.Add(state, new Dictionary<string, string>());
+        }
+        
+        for (int j = 0; j < int.Parse(statesCount); j++) {
+            foreach (string key in keys) {
+                Console.WriteLine($"{states[j]} {key}");
+                string input = Console.ReadLine();
+                foreach (var (state, dic) in adjacency) {
+                    if (state == states[j].ToString()) {
+                        adjacency[state].Add(key.ToString(), input);
+                    }
+                }
+            }
+        }
+        
+        foreach (var (a, b) in adjacency) {
+            Console.Write(a);
+            foreach (var c in b) Console.Write($" {c} ");
+                Console.WriteLine();
+            }
+        int counter = 0;
+        while (counter < 5) {
+            Console.WriteLine("Enter test string");
+            string test = Console.ReadLine();
+            DFA(adjacency, test, firstState, finalState);
+            counter++;
+        }
+    }
+    static void DFA(Dictionary <string, Dictionary<string, string>> adjacency, string input, string firstState, string finalState) {
+        string currentState = firstState;
+        foreach (char c in input) {
+            foreach (var (edge, key) in adjacency[currentState]) {
+                if (edge == c.ToString()) {
+                    currentState = key;
+                    break;
+                }
+            }
+        }
+        if (currentState == finalState) Console.WriteLine("Yes");
+        else Console.WriteLine("No");
+    }
+}
